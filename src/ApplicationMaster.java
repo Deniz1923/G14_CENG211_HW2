015d2136af8a1acc1308ff478bd5f0d59acdc58c
AlluvianDev <ar.zeytin@hotmail.com>
2025-11-04 13:25:33 +0300

    mini commit

--- src/ApplicationMaster.java
+++ src/ApplicationMaster.java
@@ -1,2 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ApplicationMaster {
+    private final ArrayList<Applicant> applicants;
+    public ApplicationMaster(ArrayList<ArrayList<String>> rawInput){
+        this.applicants = new ArrayList<>();
+        processRawInput(rawInput);
+    }
+
+    private void processRawInput(ArrayList<ArrayList<String>> rawInput){
+        //First string value
+        for(ArrayList<String> list : rawInput){
+            String id = list.get(1);
+        }
+    }
 }
