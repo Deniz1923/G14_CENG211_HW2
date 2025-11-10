f4c14fb2b7ebe47eb2677abba1b62d8cd7edd40d
Deniz1923 <ozgurdeniz2018@gmail.com>
2025-11-10 17:47:36 +0300

    Refactored Application.java

--- src/applicationtypes/Application.java
+++ src/applicationtypes/Application.java
@@ -3,21 +3,41 @@ package applicationtypes;
 import core.Applicant;
 import core.EvaluationResult;
 
+/**
+ * Abstract base class for all application types.
+ * Provides common functionality for evaluating different scholarship applications.
+ */
 abstract public class Application {
     protected Applicant applicant;
     protected EvaluationResult result;
 
+    /**
+     * Constructs an application with the given applicant.
+     * @param applicant The applicant associated with this application
+     */
     public Application(Applicant applicant) {
         this.applicant = applicant;
     }
 
-    public abstract void evaluate() ;
+    /**
+     * Evaluates the application according to its specific type.
+     * This method should be implemented by subclasses.
+     */
+    public abstract void evaluate();
 
+    /**
+     * Returns string representation of the evaluation result.
+     * @return String representation of the evaluation result
+     */
     @Override
     public String toString() {
         return result.toString();
     }
 
+    /**
+     * Gets the evaluation result for this application.
+     * @return The evaluation result
+     */
     public EvaluationResult getResult() {
         return result;
     }
